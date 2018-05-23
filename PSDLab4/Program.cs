fad9f1485d0affd922e3b2dafd1c8af7d3375e64
NickVinok <nickvinok@yandex.ru>
2018-05-23 21:25:32 +0300

    initial commit

---
+++ PSDLab4/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using 
+using System.Windows.Forms;
+using Autofac;
+
+namespace PSDLab4
+{
+    static class Program
+    {
+        private static IContainer _container;
+
+        [STAThread]
+        static void Main()
+        {
+            RegisterTypes();
+
+            var presenter = _container.Resolve<Pres>();
+            //presenter.view = _container.Resolve<MainForm>();
+            //presenter.model = _container.Resolve<Model>();
+            try
+            {
+                presenter.Run();
+            }
+            catch (Exception e)
+            {
+                Application.Exit();
+            }
+        }
+
+        private static void RegisterTypes()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<Model>().As<IModel>();
+            builder.RegisterType<MainForm>().As<IView>();
+            builder.RegisterType<Pres>();
+            _container = builder.Build();
+        }
+    }
+}
---
+++ View/Forms/MainForn.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Windows.Forms;
+using View.ViewInterfaces;
+
+namespace View.Forms
+{
+    public partial class MainForn : Form, IMainView
+    {
+        public MainForn()
+        {
+            InitializeComponent();
+        }
+    }
+}
